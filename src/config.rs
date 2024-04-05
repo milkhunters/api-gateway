95e8fbcd4e7a64e50bfd47454f7d39d627f96b26
JKearnsl <boyarshinovn@gmail.com>
2024-04-05 22:08:12 +0300

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,60 @@
+use std::fs::{File, OpenOptions};
+use std::io::Read;
+use std::io::Write;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum DistributeMode {
+    Single,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Config {
+    pub host: String,
+    pub port: u16,
+    pub mode: DistributeMode,
+    pub storages: Vec<String>,
+}
+
+impl Config {
+    pub fn new(config_path: &str) -> Result<Config, String> {
+
+        if !std::path::Path::new(config_path).exists() {
+            let default_config = Config {
+                host: "127.0.0.1".to_string(),
+                port: 8080,
+                mode: DistributeMode::Single,
+                storages: vec![],
+            };
+
+            match OpenOptions::new().write(true).create_new(true).open(config_path) {
+                Ok(mut file) => {
+                    if let Err(error) = writeln!(file, "{}", serde_yaml::to_string(&default_config).unwrap()) {
+                        return Err(format!("Failed to write to config.yaml -> {}", error));
+                    }
+                },
+                Err(error) => return Err(format!("Failed to create config.yaml -> {}", error)),
+            };
+        }
+
+
+        let mut file = match File::open(config_path) {
+            Ok(file) => file,
+            Err(error) => return Err(format!("Failed to open config.yaml -> {}", error)),
+        };
+        let mut contents = String::new();
+        match file.read_to_string(&mut contents) {
+            Ok(_) => (),
+            Err(error) => return Err(format!("Failed to read config.yaml -> {}", error)),
+        };
+
+        let config: Config = match serde_yaml::from_str(&contents) {
+            Ok(config) => config,
+            Err(error) => {
+                return Err(format!("Failed to parse config.yaml -> {}", error));
+            },
+        };
+        Ok(config)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use actix_web::{
+    App,
+    HttpServer,
+    middleware::Logger,
+    web
+};
+
+mod routes;
+mod models;
+mod config;
+mod core;
+
+struct AppState {
+    config: config::Config,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let config = match config::Config::new("config.yaml") {
+        Ok(config) => config,
+        Err(error) => {
+            log::error!("{}", error);
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, error));
+        },
+    };
+    let host = config.host.clone();
+    let port = config.port.clone();
+
+    log::info!("Starting server at http://{}:{}", host, port);
+
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(AppState {
+            config: config.clone(),
+        })
+        ).service(web::scope("/api/v1")
+            .configure(routes::api::auth::router)
+        ).service(web::scope("/iface")
+            .configure(routes::coral::router)
+        )
+            .wrap(Logger::new("[%s] [%{r}a] %U"))
+    }).bind((host, port))?.run().await
+}
